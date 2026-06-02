263ea986a336fe3cb668e01ad857ab2771d34ca3
//. <174224977+skye-tachyon@users.noreply.github.com>
2026-06-02 12:31:36 -0400

    sm8250-common: implement usage of dex2oat boot image profile
    Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -33,6 +33,10 @@ PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
+# Use a profile based boot image for this device
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
+
 # Dex
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Launcher3QuickStep \
